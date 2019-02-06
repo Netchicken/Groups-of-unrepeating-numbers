01b51254422e0d7ac9a2b02335ad35debe8c82b8
Gary Dix <intelproof@gmail.com>
2019-02-06 13:57:15 +1300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CounsellingTriads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSessionlength = new System.Windows.Forms.TextBox();
+            this.txtStudentCount = new System.Windows.Forms.TextBox();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbUniques = new System.Windows.Forms.RadioButton();
+            this.rbNonUnique = new System.Windows.Forms.RadioButton();
+            this.gbTypes = new System.Windows.Forms.GroupBox();
+            this.rbnRnd = new System.Windows.Forms.RadioButton();
+            this.gbTypes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSessionlength
+            // 
+            this.txtSessionlength.Location = new System.Drawing.Point(798, 206);
+            this.txtSessionlength.Name = "txtSessionlength";
+            this.txtSessionlength.Size = new System.Drawing.Size(372, 49);
+            this.txtSessionlength.TabIndex = 0;
+            // 
+            // txtStudentCount
+            // 
+            this.txtStudentCount.Location = new System.Drawing.Point(1288, 206);
+            this.txtStudentCount.Name = "txtStudentCount";
+            this.txtStudentCount.Size = new System.Drawing.Size(403, 49);
+            this.txtStudentCount.TabIndex = 1;
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.ItemHeight = 42;
+            this.lbxOutput.Location = new System.Drawing.Point(110, 169);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.ScrollAlwaysVisible = true;
+            this.lbxOutput.Size = new System.Drawing.Size(589, 886);
+            this.lbxOutput.TabIndex = 2;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(81, 61);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(607, 83);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "button1";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(787, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 44);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1301, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // rbUniques
+            // 
+            this.rbUniques.AutoSize = true;
+            this.rbUniques.Checked = true;
+            this.rbUniques.Location = new System.Drawing.Point(45, 86);
+            this.rbUniques.Name = "rbUniques";
+            this.rbUniques.Size = new System.Drawing.Size(319, 48);
+            this.rbUniques.TabIndex = 6;
+            this.rbUniques.TabStop = true;
+            this.rbUniques.Text = "Unique Groups";
+            this.rbUniques.UseVisualStyleBackColor = true;
+            this.rbUniques.CheckedChanged += new System.EventHandler(this.rbUniques_CheckedChanged);
+            // 
+            // rbNonUnique
+            // 
+            this.rbNonUnique.AutoSize = true;
+            this.rbNonUnique.Location = new System.Drawing.Point(45, 184);
+            this.rbNonUnique.Name = "rbNonUnique";
+            this.rbNonUnique.Size = new System.Drawing.Size(254, 48);
+            this.rbNonUnique.TabIndex = 7;
+            this.rbNonUnique.Text = "Non Unque";
+            this.rbNonUnique.UseVisualStyleBackColor = true;
+            this.rbNonUnique.CheckedChanged += new System.EventHandler(this.rbNonUnique_CheckedChanged);
+            // 
+            // gbTypes
+            // 
+            this.gbTypes.Controls.Add(this.rbnRnd);
+            this.gbTypes.Controls.Add(this.rbNonUnique);
+            this.gbTypes.Controls.Add(this.rbUniques);
+            this.gbTypes.Location = new System.Drawing.Point(1288, 313);
+            this.gbTypes.Name = "gbTypes";
+            this.gbTypes.Size = new System.Drawing.Size(419, 486);
+            this.gbTypes.TabIndex = 8;
+            this.gbTypes.TabStop = false;
+            this.gbTypes.Text = "Types Of Groups";
+            // 
+            // rbnRnd
+            // 
+            this.rbnRnd.AutoSize = true;
+            this.rbnRnd.Location = new System.Drawing.Point(45, 275);
+            this.rbnRnd.Name = "rbnRnd";
+            this.rbnRnd.Size = new System.Drawing.Size(208, 48);
+            this.rbnRnd.TabIndex = 8;
+            this.rbnRnd.Text = "Random";
+            this.rbnRnd.UseVisualStyleBackColor = true;
+            this.rbnRnd.CheckedChanged += new System.EventHandler(this.rbnRnd_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1960, 1202);
+            this.Controls.Add(this.gbTypes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.txtStudentCount);
+            this.Controls.Add(this.txtSessionlength);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbTypes.ResumeLayout(false);
+            this.gbTypes.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSessionlength;
+        private System.Windows.Forms.TextBox txtStudentCount;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbUniques;
+        private System.Windows.Forms.RadioButton rbNonUnique;
+        private System.Windows.Forms.GroupBox gbTypes;
+        private System.Windows.Forms.RadioButton rbnRnd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CounsellingTriads
+{
+    public partial class Form1 : Form
+    {
+        private Students myStudents;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Instantiate the lists = names
+            for (int i = 1; i < 22; i++)
+            {
+                myStudents.StudentHistory[i] = new List<string>();
+            }
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            lbxOutput.Items.Clear();
+
+            GenerateGroups();
+        }
+
+
+        private void GenerateGroups()
+        {
+            //reinstantiate everying to clear out old data
+            myStudents = new Students();
+
+            //need to have a list of all students to record all students they have been in a group with
+            //output to date = 25 groups
+
+            //All students have a list that contains all the students they have been in a group with before.
+
+
+
+
+            for (int k = 1; k < 21; k++) //get all permutations
+            {
+                for (int i = k; i < 22; i++)
+                {
+                    myStudents.student = null;
+
+                    //if random then generate rnd student number and use that
+                    if (myStudents.IsRnd == true)
+                    {
+                        myStudents.student = myStudents.MyRnd.Next(1, 22).ToString();
+                    }
+                    else
+                    {
+                        myStudents.HasOneStudentBeenAdded = false;
+                        myStudents.student = i.ToString(); //iterate though for loop
+                        myStudents.StudentToAdd = null;
+                    }
+                    //Need to add the first member manually or it crashes at the foreach
+
+                    if (myStudents.singlegroup.Count == 0)
+                    {
+                        myStudents.singlegroup.Add(myStudents.student);
+                        myStudents.StudentHistory[1].Add(myStudents.student);
+                    }
+                    else
+                    {
+                        myStudents.HasStudentBeenBefore = false;
+
+                        GenerateMembers();
+
+                        //you can have a pair of people in a group with history
+                        if (myStudents.IsUnique == false && myStudents.IsRnd == false)
+                        {
+
+                            //add the student to the group HasStudentBeenBefore == true &&
+                            if (myStudents.HasOneStudentBeenAdded == false)
+                            {
+                                myStudents.HasOneStudentBeenAdded = true;
+                                myStudents.singlegroup.Add(myStudents.StudentToAdd);
+                            }
+                        }
+
+                        else
+                        {
+                            //Uniques Only
+                            //add the student to the group
+                            if (myStudents.HasStudentBeenBefore == false)
+                            {
+                                myStudents.singlegroup.Add(myStudents.StudentToAdd);
+                            }
+                        }
+
+
+
+                        //if the group has reached 3 then print it and clear it for the next group
+
+                        if (myStudents.singlegroup.Count == 3)
+                        {
+                            String Group = null;
+                            myStudents.HasOneStudentBeenAdded = false; // reset for new group
+                            foreach (var item in myStudents.singlegroup)
+                            {
+                                Group += item + " ";
+                            }
+                            myStudents.Countgroups++;
+                            lbxOutput.Items.Add(Group);
+                            myStudents.singlegroup.Clear(); //empty the list ready for the next one
+                        }
+                    }
+
+                }
+                //  count++;
+                this.Text = myStudents.Countgroups.ToString();
+                // }
+            }
+        }
+
+        private void GenerateMembers()
+        {
+            foreach (var AGroupStudent in myStudents.singlegroup) // for each person in the group
+            { //convert the person to an int to find their history list
+                int GroupStudent = Convert.ToInt32(AGroupStudent);
+
+                if (!myStudents.StudentHistory[GroupStudent].Contains(myStudents.student))
+                {
+                    //if the student isn't in the group, and they have never been with the others then add them
+                    if (myStudents.HasStudentBeenBefore == false)
+                    {
+                        myStudents.StudentToAdd = myStudents.student;
+                    }
+                    //add that person to the history of the others
+                    myStudents.StudentHistory[GroupStudent].Add(myStudents.student);
+                }
+                else //they have met that person before
+                {
+                    myStudents.HasStudentBeenBefore = true;
+
+
+                    ////add the student to the list to be saved if the non unique is true
+                    //todo what the heck was I thinking here?
+                    //  if (IsUnique == true)
+                    //  {
+                    myStudents.StudentToAdd = myStudents.student;
+                    //  }
+                    //  break; //stop this foreach and go back to For
+                }
+            }
+        }
+
+        public void RandomSelection()
+        {
+            bool isFindingAmatch = true;
+
+
+            if (myStudents.singlegroup.Count == 0)
+            {
+                myStudents.singlegroup.Add(myStudents.student);
+                myStudents.StudentHistory[1].Add(myStudents.student);
+            }
+
+            while (isFindingAmatch)
+            {
+                myStudents.student = myStudents.MyRnd.Next(1, 22).ToString();
+
+            }
+
+
+
+
+        }
+
+        private void rbUniques_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (rbUniques.Checked == true)
+            {
+                myStudents.IsUnique = true;
+                lbxOutput.Items.Clear();
+            }
+        }
+
+        private void rbNonUnique_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbNonUnique.Checked == true)
+            {
+                myStudents.IsUnique = false;
+                lbxOutput.Items.Clear();
+            }
+
+        }
+
+        private void rbnRnd_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbNonUnique.Checked == true)
+            {
+                myStudents.IsRnd = true;
+
+                lbxOutput.Items.Clear();
+            }
+            else
+            {
+                myStudents.IsRnd = false;
+            }
+        }
+    }
+
+
+    internal class Group
+    {
+        public List<string> AllTeams = new List<string>();
+    }
+    internal class Students
+    {
+        public List<string> singlegroup = new List<string>();
+        public List<string>[] StudentHistory = new List<string>[22];
+        public List<string> studentHistory = new List<string>();
+
+        public bool IsUnique = true; //Unique or NonUnique groups
+        public bool IsRnd = false; //using the rnd radiobutton
+        public bool HasStudentBeenBefore = false;
+        public bool HasOneStudentBeenAdded = false;
+        public string student = null;
+        public string StudentToAdd = null;
+
+        public int Countgroups = 0;
+        public int CountStudents = 21;
+        public Random MyRnd = new Random();
+    }
+}
