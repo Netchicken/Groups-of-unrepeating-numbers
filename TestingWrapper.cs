cc20b8c31f067c231e58fa7b3189cba2a941a3bb
Gary Dix <intelproof@gmail.com>
2019-02-13 15:27:09 +1300

    Unit Testing

---
+++ TestingWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounsellingTriads
+{
+    /// <summary>
+    /// You can't easily test static methods. Therefore if I wrap them in a class and instantiate that I can test the output of the static class.
+    /// 
+    /// </summary>
+    public class TestingWrapper
+    {
+
+        public int CountNumbersGenerated()
+        {
+            return Operations.StudentNames.Count;
+
+        }
+
+        public int CountUniquesGenerated(Students myStudents)
+        {
+
+            return GenerateStudentGroups.GenerateGroups(myStudents).Count();
+        }
+
+    }
+}
